5331edea129c101d75cf5881f265c5da7bf7d774
Daniel Alvarez <a3dany@gmail.com>
2017-01-29 22:10:23 -0400

    first commit

---
+++ src/tech/alvarez/grammarcleaner/Main.java
@@ -0,0 +1,14 @@
+package tech.alvarez.grammarcleaner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Grammar G = new Grammar();
+//        G.adicionarProduccion(new Production('S', "S"));
+//        G.limpiar();
+//        G.mostrar();
+
+        MainFrame V = new MainFrame();
+        V.setVisible(true);
+    }
+}
